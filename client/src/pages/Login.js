d189d159dfdc7577200c7a5ab715b46c41f1c802
Pasindujay1 <pasindujayasinghe@gmail.com>
2023-08-15 09:56:52 +0530

    login frontend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () =>{
+
+    const [credentials ,setCredentials] = useState({
+        email:"",
+        password:""
+    })
+    //function to change state with user inputs
+    const handleInputChange = (event) =>{
+        const {name, value} = event.target;
+
+        //to change the state of our login page
+        //changing the previous state with the new state
+        setCredentials({...credentials, [name]:value});
+         
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefault(); //prevent the page from refreshing on submit
+    }
+
     return <>
     <h3>Login</h3>
 
-    <form>
+    <form onSubmit={handleSubmit}>
         <div class="form-group">
         <label for="emailInput" class="form-label mt-4">Email address</label>
-        <input type="email" class="form-control" id="emailInput" name="email" aria-describedby="emailHelp" placeholder="simon@example.com"/>
+        <input type="email" class="form-control"  value={credentials.email} id="emailInput" onChange={handleInputChange} name="email" aria-describedby="emailHelp" placeholder="simon@example.com" required/>
         </div>
         <div class="form-group">
         <label for="password" class="form-label mt-4">Password</label>
-        <input type="password" class="form-control" id="password" name="email"  placeholder="Enter password"/>
+        <input type="password" class="form-control" value={credentials.password} id="password" onChange={handleInputChange} name="password"  placeholder="Enter password" required/>
         </div>
-        <input type="submit" value="Login" className="btn btn-primary" />
+        <input type="submit" value="Login" className="btn btn-primary my-3" />
+        <p>Don't have an account ? <Link to="/Register">Create One</Link></p>
     </form>
     </>
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Register = () =>{
-    return <>This is Register Page</>
+    const [credentials, setCredentials] = useState({
+        email:"",
+        password:"",
+        confirmPassword:""
+
+});
+const handleInputChange = (event) =>{
+    const {name,value} = event.target;
+
+    setCredentials({...credentials,[name]:value }); //we create a shallow copy of the current credentials object and update the relevent property
+};
+
+const handleSubmit = (event) =>{
+    event.preventDefault(); //to perevent the page from being refreshed when Register button is clicked
+}
+
+    return (
+    <>
+    <h3>Create your account</h3>
+    
+     <form onSubmit={handleSubmit}>
+        <div class="form-group">
+        <label for="emailInput" class="form-label mt-4">Email address</label>
+        <input type="email" class="form-control" id="emailInput" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={handleInputChange} placeholder="simon@example.com" required/>
+        </div>
+        <div class="form-group">
+        <label for="password" class="form-label mt-4">Password</label>
+        <input type="password" class="form-control" id="password" name="password" value={credentials.password} onChange={handleInputChange} placeholder="Enter password" required/>
+        </div>
+        <div class="form-group">
+        <label for="confirmPassword" class="form-label mt-4">Confirm Password</label>
+        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" value={credentials.confirmPassword} onChange={handleInputChange} placeholder="Enter password" required/>
+        </div>
+
+        <input type="submit" value="Register" className="btn btn-primary my-3" />
+        <p>Already have an account ? <Link to="/Login">Login</Link></p>
+    </form>
+    </>
+    );
 };
 
 export default Register;
